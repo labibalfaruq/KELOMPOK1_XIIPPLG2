3bf058a109781d51f11956afc0b88a3c8761e6d6
salsanasywa <salsanasywa3@gmail.com>
2023-10-13 10:05:26 +0700

    entity menu

---
+++ app/src/main/java/com/kelompok1_xiipplg2/ROOM/menu.kt
@@ -0,0 +1,17 @@
+package com.kelompok1_xiipplg2.ROOM
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity (tableName = "menucafe")
+class menu (
+    @PrimaryKey (autoGenerate = false)
+    @ColumnInfo (name = "americano") val americano:String,
+    @ColumnInfo (name ="arabika" )val arabika :String,
+    @ColumnInfo (name = "cookies")val cookies :String,
+    @ColumnInfo (name = "kopi susu")val kopi_susu:String,
+    @ColumnInfo (name = "mie goreng")val mie_goreng : String,
+    @ColumnInfo (name = "french fries")val french_fries: String,
+)
