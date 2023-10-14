f6c4aef393d4413fda8afe62fef3c040e3d31c21
labibalfaruq <pisuke534@gmail.com>
2023-10-14 09:06:25 +0700

    login

--- app/src/main/java/com/kelompok1_xiipplg2/activity_login.kt
+++ app/src/main/java/com/kelompok1_xiipplg2/activity_login.kt
@@ -1,11 +1,43 @@
 package com.kelompok1_xiipplg2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kelompok1_xiipplg2.databinding.ActivityLoginBinding
 
 class activity_login : AppCompatActivity() {
+    private lateinit var bind:ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        bind=ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(bind.root)
+        bind.btnlogin.setOnClickListener{
+            startActivity(
+                Intent(
+                    this,profil::class.java
+                )
+            )
+
+        }
+        fun login(){
+            val validusername="username123"
+            val validpassword="password123"
+
+            println("mohon isi username")
+            val enterusername = readLine()
+
+            println("isi passwordmu")
+            val enterpassword = readLine()
+
+            if (enterusername==validusername && enterpassword==validpassword){
+                println("login berhasil")
+            }else{
+                println("login failed")
+            }
+        }
+
+
         setContentView(R.layout.activity_login)
+
     }
 }
