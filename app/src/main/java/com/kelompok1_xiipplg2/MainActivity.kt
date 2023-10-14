698b1970656d92f6f2cf7ecd75ee7c29c3d55d5f
essacitra <essacitraadelia@gmail.com>
2023-10-14 07:48:58 +0700

    SplashScreen

--- app/src/main/java/com/kelompok1_xiipplg2/MainActivity.kt
+++ app/src/main/java/com/kelompok1_xiipplg2/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.kelompok1_xiipplg2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        Handler(Looper.getMainLooper()).postDelayed({
+            println("RUN")
+            val intent = Intent(this, profil::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
     }
 }
