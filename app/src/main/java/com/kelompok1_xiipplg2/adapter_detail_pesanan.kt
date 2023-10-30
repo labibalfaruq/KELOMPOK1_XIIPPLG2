4928ba3e06c8ac91988c5a663ae14227bf2a9474
iffahmufidah <iffamufidah1805>
2023-10-30 10:11:52 +0700

    adapter

---
+++ app/src/main/java/com/kelompok1_xiipplg2/adapter_detail_pesanan.kt
@@ -0,0 +1,11 @@
+package com.kelompok1_xiipplg2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class adapter_detail_pesanan : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_adapter_detail_pesanan)
+    }
+}
