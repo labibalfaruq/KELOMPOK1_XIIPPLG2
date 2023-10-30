c9a7c68592658a8bf52a14cf4d54b96b491f2c49
salsanasywa <salsanasywa3@gmail.com>
2023-10-30 08:14:00 +0700

    tambahan karyawan

--- app/src/main/java/com/kelompok1_xiipplg2/input_karyawan.kt
+++ app/src/main/java/com/kelompok1_xiipplg2/input_karyawan.kt
@@ -3,17 +3,38 @@ package com.kelompok1_xiipplg2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.kelompok1_xiipplg2.ROOM.database
+import com.kelompok1_xiipplg2.ROOM.tbkaryawan
 import com.kelompok1_xiipplg2.databinding.ActivityInputKaryawanBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class input_karyawan : AppCompatActivity() {
 
     private lateinit var binding: ActivityInputKaryawanBinding
-    private lateinit var adapter:adapterkaryawan
     private val db by lazy { database(this) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding= ActivityInputKaryawanBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btntambah.setOnClickListener{
+            datakaryawan()
+        }
 
+    }
 
-        setContentView(R.layout.activity_input_karyawan)
+    private fun datakaryawan() {
+      CoroutineScope(Dispatchers.IO).launch {
+          db.karyawandao().simpan_data(
+              tbkaryawan(
+                  0,
+                  binding.namakaryawan.text.toString(),
+                  binding.TTL.text.toString(),
+                  binding.alamat.text.toString(),
+                  binding.rd.toString()
+              )
+          )
+          finish()
+      }
     }
 }
